bc909fe2f6b7a3f7daae8564826248e7fe887793
renatoabreu11 <renatoabreu1196@gmail.com>
2017-02-25 17:14:56 +0000

    MulticastSocketServer completed

--- UDP Multicast/MulticastSocketClient.java
+++ UDP Multicast/MulticastSocketClient.java
@@ -1,17 +1,69 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
 public class MulticastSocketClient {
     public static void main(String[] args) throws Exception {
         if (args.length < 4) {
-            System.out.println("Usage: java MulticastSocketClient <mcast_addr> <mcast_port> <oper> <opnd>*");
+            System.out.println("Usage: java MulticastSocketClient1 <mcast_addr> <mcast_port> <oper> <opnd>*");
             return;
         }
 
+        String mcast_addr = args[0];
+        int mcast_port = Integer.parseInt(args[1]);
         String pattern = "\\w{2}-\\w{2}-\\w{2}";
         String request = args[2];
         String plate_number = args[3];
+        String message;
 
-        if(!plate_number.matches(pattern)){
+        if (!plate_number.matches(pattern)) {
             System.out.println("The plate number must be in the format XX-XX-XX where X is a letter or a digit.");
             return;
+        }
+
+        switch (request) {
+            case "register":
+                String owner_name = args[4];
+                if (owner_name.length() > 256) {
+                    System.out.println("The vehicle owner's name must have less than 256 characters.");
+                    return;
+                } else message = "REGISTER " + plate_number + " " + owner_name;
+                break;
+            case "lookup":
+                message = "LOOKUP " + plate_number;
+                break;
+            default:
+                System.out.print("Invalid MulticastSocketClient Request. It has to be register or lookup.");
+                return;
+        }
+        byte[] buffer = message.getBytes();
+
+        //One would join a multicast group by first creating a MulticastSocket with the desired port,
+        //then invoking the joinGroup(InetAddress groupAddr) method
+        InetAddress group = InetAddress.getByName(mcast_addr);
+        try (MulticastSocket clientSocket = new MulticastSocket(mcast_port)) {
+            clientSocket.joinGroup(group);
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, group, mcast_port);
+            clientSocket.send(packet);
+
+            byte[] rbuffer = new byte[1024];
+            packet = new DatagramPacket(rbuffer, rbuffer.length, group, mcast_port);
+            clientSocket.receive(packet);
+            String received = new String(packet.getData(), 0, packet.getLength());
+            System.out.println("Echoed Message: " + received);
+            if(received.matches("^-?\\d+$"))
+                if(received.equals("-1"))
+                    System.out.println("Plate number " + plate_number + " has already been registered!");
+                else System.out.println("Plate number " + plate_number + " registered!" + " The current number of vehicles registered is " + received + ".");
+            else if(received.equals("NOT_FOUND"))
+                System.out.println("The plate number " + plate_number + " isn't registered in the system!");
+            else System.out.println(received + " is the owner of the vehicle with plate number " + plate_number + ".");
+            clientSocket.leaveGroup(group);
+            clientSocket.close();
+            System.out.println("Socket closed!");
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 }
--- UDP Multicast/MulticastSocketServer.java
+++ UDP Multicast/MulticastSocketServer.java
@@ -1,8 +1,88 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MulticastSocketServer {
+
     public static void main(String[] args) throws Exception {
         if (args.length != 3) {
-            System.out.println("Usage: java MulticastSocketServer <srvc_port> <mcast_addr> <mcast_port>");
+            System.out.println("Usage: java MulticastSocketServer1 <srvc_port> <mcast_addr> <mcast_port>");
             return;
+        }
+
+        int srvc_port = Integer.parseInt(args[0]);
+        int mcast_port = Integer.parseInt(args[2]);
+        String mcast_addr = args[1];
+        InetAddress addr = InetAddress.getByName(mcast_addr);
+        DatagramSocket serverSocket = new DatagramSocket();
+
+        String advertisement = "<" + mcast_addr + "><" + args[2] + ">:<" + "localhost><" + args[0] + ">";
+        byte[] adMessage = advertisement.getBytes();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    DatagramPacket resp = new DatagramPacket(adMessage, adMessage.length, addr, mcast_port);
+                    serverSocket.send(resp);
+                    System.out.println("multicast:"+advertisement);
+                } catch (IOException e) {
+
+                }
+            }
+        };
+
+        Timer timer = new Timer("MyTimer");//create a new Timer
+        timer.scheduleAtFixedRate(timerTask, 1000, 1000);
+
+        byte[] buffer = new byte[1024];
+        DatagramPacket dgp = new DatagramPacket(buffer, buffer.length);
+        System.out.println("SocketServer initialized!");
+        Map<String, String> vehicles = new HashMap<>();
+
+        while (true) {
+            serverSocket.receive(dgp);
+            String request = new String(dgp.getData(), 0, dgp.getLength());
+            String message = request + ", from address: " + dgp.getAddress() + ", through port: " + dgp.getPort();
+            System.out.println(message);
+            String[] splitedRequest = request.split("\\s+");
+
+            String response = null;
+            String plate_number = splitedRequest[1];
+            if (splitedRequest[0].equals("REGISTER")) {
+                String lookup = vehicles.get(plate_number);
+                String owner_name = "";
+                for (int i = 2; i < splitedRequest.length; i++) {
+                    if (i == 2)
+                        owner_name = owner_name + splitedRequest[i];
+                    else owner_name = owner_name + " " + splitedRequest[i];
+                }
+                if (lookup != null) {
+                    response = "-1";
+                    System.out.println("<Register><" + plate_number + "><" + owner_name + ">::-1");
+                } else {
+                    vehicles.put(plate_number, owner_name);
+                    response = Integer.toString(vehicles.size());
+                    System.out.println("<Register><" + plate_number + "><" + owner_name + ">::"+response);
+                }
+            } else if (splitedRequest[0].equals("LOOKUP")) {
+                String lookup = vehicles.get(plate_number);
+                if (lookup == null) {
+                    response = "NOT_FOUND";
+                    System.out.println("<Lookup><" + plate_number + ">::NOT_FOUND");
+                } else {
+                    response = lookup;
+                    System.out.println("<Lookup><" + plate_number + ">::" + lookup);
+                }
+            }
+
+            buffer = response.getBytes();
+            DatagramPacket resp = new DatagramPacket(buffer, buffer.length, addr, mcast_port);
+            serverSocket.send(resp);
         }
     }
 }
