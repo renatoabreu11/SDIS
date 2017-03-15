e74b158ea6566b8383d91ac0ac090030f1d2e7c4
renatoabreu11 <renatoabreu1196@gmail.com>
2017-03-15 16:28:17 +0000

    Splitter and chunk functionalities implemented

--- src/fileSystem/Chunk.java
+++ src/fileSystem/Chunk.java
@@ -1,4 +1,61 @@
 package fileSystem;
 
-public class Chunk {
+public class Chunk implements Comparable<Chunk> {
+
+    private int chunkNo;
+    private int replicationDegree;
+    private int currReplicationDegree;
+    private byte[] chunkData;
+
+    public Chunk(int replicationDegree, int chunkNo, byte[] chunkData){
+        this.chunkNo = chunkNo;
+        this.replicationDegree = replicationDegree;
+        this.currReplicationDegree = 0;
+        this.chunkData = chunkData;
+    }
+
+    @Override
+    public int compareTo(Chunk chunk) {
+        return 0;
+    }
+
+    public int getChunkNo() {
+        return chunkNo;
+    }
+
+    public void setChunkNo(int chunkNo) {
+        this.chunkNo = chunkNo;
+    }
+
+    public byte[] getChunkData() {
+        return chunkData;
+    }
+
+    public void setChunkData(byte[] chunkData) {
+        this.chunkData = chunkData;
+    }
+
+    public int getReplicationDegree() {
+        return replicationDegree;
+    }
+
+    public void setReplicationDegree(int replicationDegree) {
+        this.replicationDegree = replicationDegree;
+    }
+
+    public int getCurrReplicationDegree() {
+        return currReplicationDegree;
+    }
+
+    public void setCurrReplicationDegree(int currReplicationDegree) {
+        this.currReplicationDegree = currReplicationDegree;
+    }
+
+    public void addReplicationDegree(){
+        this.currReplicationDegree++;
+    }
+
+    public void subReplicationDegree(){
+        this.currReplicationDegree--;
+    }
 }
--- src/fileSystem/Splitter.java
+++ src/fileSystem/Splitter.java
@@ -1,4 +1,82 @@
 package fileSystem;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static java.util.Arrays.copyOfRange;
+
 public class Splitter {
+    private String fileName;
+    private ArrayList<Chunk> chunks;
+
+    /**
+     * @param fileName file full path!
+     */
+    public Splitter(String fileName){
+        this.fileName = fileName;
+        this.chunks = new ArrayList<>();
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public ArrayList<Chunk> getChunks() {
+        return chunks;
+    }
+
+    public void setChunks(ArrayList<Chunk> chunks) {
+        this.chunks = chunks;
+    }
+
+    public void resetSplitter(){
+        fileName = null;
+        chunks.clear();
+    }
+
+    public void splitFile(int desiredReplicationDegree) throws IOException {
+        Path path = Paths.get(this.fileName);
+        byte[] data = Files.readAllBytes(path);
+
+        int currentChunkNo = 0;
+
+        if(data.length <= 64000){
+            Chunk c = new Chunk(desiredReplicationDegree, currentChunkNo, data);
+            chunks.add(c);
+        }else{
+            for(int i = 0; i < data.length; i++){
+                if( i != 0 && i % 64000 == 0){
+                    currentChunkNo++;
+                    int minDelimiter = i - 64000;
+                    int maxDelimiter = i;
+                    byte[] chunkData = copyOfRange(data, minDelimiter, maxDelimiter);
+                    Chunk c = new Chunk(desiredReplicationDegree, currentChunkNo, chunkData);
+                    System.out.println(chunkData.length);
+                    System.out.println(Arrays.toString(chunkData));
+                    chunks.add(c);
+                }
+            }
+
+            int nrChunks = currentChunkNo;
+            int leftoverBytes = data.length - (nrChunks * 64000);
+            if(leftoverBytes != 0){
+                currentChunkNo++;
+                int minDelimiter = data.length - leftoverBytes;
+                int maxDelimiter = data.length;
+                byte[] chunkData = copyOfRange(data, minDelimiter, maxDelimiter);
+                Chunk c = new Chunk(desiredReplicationDegree, currentChunkNo, chunkData);
+                System.out.println(chunkData.length);
+                System.out.println(Arrays.toString(chunkData));
+                chunks.add(c);
+            }
+        }
+    }
 }
