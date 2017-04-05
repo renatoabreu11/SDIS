100a094b883452d83c66f47023616ab6b78fa378
renatoabreu11 <renatoabreu1196@gmail.com>
2017-04-05 15:28:00 +0100

    minor fix

--- src/network/Peer.java
+++ src/network/Peer.java
@@ -252,12 +252,7 @@ public class Peer implements IClientPeer {
 
     public synchronized boolean freeDisposableSpace(long spaceNeeded) {
         ArrayList<Chunk> storedChunksWithHighRD = manager.getChunksWithHighRD(id);
-
-        storedChunksWithHighRD.sort((s1, s2) -> {
-            if (s1.getReplicationDegree() > s2.getReplicationDegree()) return 1;
-            else return 0;
-        });
-
+        
         if(storedChunksWithHighRD.size() == 0)
             return false;
 
